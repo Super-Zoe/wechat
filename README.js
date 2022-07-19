44d77c95e22399f77fac48dd2a19fd413d23e646
Zoe <39052286+Super-Zoe@users.noreply.github.com>
2022-07-19 17:07:22 +0800

    Update README.js

--- README.js
+++ README.js
@@ -1,6 +1,6 @@
 javascript:(function(){
   var youtube = 'https://www.youtube.com/results?search_query=';
-  var bilibili= 'https://search.bilibili.com/all?keyword='
+  var bilibili= 'https://search.bilibili.com/all?keyword=';
   var query = prompt('video query');
   if(query != '') {
     document.location.href = youtube + query;
