b930cac378f37b3a1de3fcbc989ec89fa5410ef3
Zoe <39052286+Super-Zoe@users.noreply.github.com>
2022-07-19 17:17:21 +0800

    Update README.js

--- README.js
+++ README.js
@@ -1,8 +1,16 @@
 javascript:(function(){
   var youtube = 'https://www.youtube.com/results?search_query=';
   var bilibili= 'https://search.bilibili.com/all?keyword=';
+  var TecentVideo= 'https://v.qq.com/x/search/?q=';
+  var iQiYi='https://so.iqiyi.com/so/q_';
+  var YouKu= 'https://so.youku.com/search_video/q_';
   var query = prompt('video query');
+  
   if(query != '') {
     window.open(youtube + query);
+    window.open(bilibili + query);
+    window.open(TecentVideo + query);
+    window.open(YouKu + query);
+    window.open(iQiYi + query);
   }
 })();
